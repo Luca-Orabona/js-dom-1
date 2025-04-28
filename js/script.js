b4d3a62064593f10fa37e5ee67d57e2c5ae8f025
Luca-Orabona <lucaorabona94@gmail.com>
2025-04-28 18:46:13 +0200

    refactor: exercise completed

--- js/script.js
+++ js/script.js
@@ -2,21 +2,23 @@ const btnLampadina = document.querySelector(".btn-lampadina");
 
 const lampadina = document.getElementById("lampadina");
 
+
+let accesa = false;
+
 btnLampadina.addEventListener("click", function () {
     
+    if (accesa) {
 
-
-    if (lampadina.src === "http://127.0.0.1:5500/img/white_lamp%201.png") {
-
-        lampadina.src = "./img/yellow_lamp 1.png";
-
-        btnLampadina.innerText = "Spegni";
-
+        lampadina.src = "./img/white_lamp 1.png";
+        btnLampadina.innerText = "Accendi"
+        accesa = false;
 
     } else {
 
-        lampadina.src = './img/white_lamp 1.png';
-
-        btnLampadina.innerText = "Accendi";
+        lampadina.src = "./img/yellow_lamp 1.png";
+        btnLampadina.innerText = "Spegni"
+        accesa = true;
     }
+    
+    
 });
